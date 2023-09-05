cca8d99b1f8bd97e664a5cbe5c05902a0d24f376
Jonas509 <jonasstvil12@gmail.com>
2023-09-05 00:15:17 -0400

    new commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,25 @@ import { OrderModule } from './order/order.module';
 import { OrderDetailModule } from './order-detail/order-detail.module';
 import { CartModule } from './cart/cart.module';
 import { CartItemModule } from './cart-item/cart-item.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule, BookModule, OrderModule, OrderDetailModule, CartModule, CartItemModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'Jost3476',
+      database: 'dbhtlibrary',
+      entities: [],
+      
+    }), UserModule, BookModule, OrderModule, OrderDetailModule, CartModule, CartItemModule],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
+
+
+
