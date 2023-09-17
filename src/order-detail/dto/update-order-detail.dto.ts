7c124c7230bedce08592f85a063c1af7b4025ae0
Jonas St vil <109557150+Jonas509@users.noreply.github.com>
2023-09-17 18:54:11 -0400

    Delete src directory

--- src/order-detail/dto/update-order-detail.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateOrderDetailDto } from './create-order-detail.dto';
-
-export class UpdateOrderDetailDto extends PartialType(CreateOrderDetailDto) {}
